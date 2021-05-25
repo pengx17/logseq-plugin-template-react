45bef0699eed357619e71c31326f2fa0612eaebf
Peng Xiao <pengxiao@freewheel.tv>
2021-05-25 16:50:30 +0800

    fix hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import React, { useRef, useState } from "react";
-
-const useAppVisible = () => {
-  const [visible, setVisible] = useState(logseq.isMainUIVisible);
-  React.useEffect(() => {
-    const eventName = "ui:visible:changed";
-    const handler = async ({ visible }: any) => {
-      setVisible(visible);
-    };
-    logseq.on(eventName, handler);
-    return () => {
-      logseq.off(eventName, handler);
-    };
-  }, []);
-  return visible;
-};
+import React, { useRef } from "react";
+import { useAppVisible } from "./utils";
 
 function App() {
   const innerRef = useRef<HTMLDivElement>(null);
   const visible = useAppVisible();
   if (visible) {
     return (
-      <main
-        className="backdrop-filter backdrop-blur-md top-0 bottom-0 left-0 right-0 fixed flex items-center justify-center"
+      <main        
+        className="backdrop-filter backdrop-blur-md fixed inset-0 flex items-center justify-center"
         onClick={(e) => {
           if (!innerRef.current?.contains(e.target as any)) {
             window.logseq.hideMainUI();
@@ -29,7 +15,7 @@ function App() {
         }}
       >
         <div ref={innerRef} className="text-size-2em">
-          Welcome to Logseq Plugins!
+          Welcome to [[Logseq]] Plugins!
         </div>
       </main>
     );
